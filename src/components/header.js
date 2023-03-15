e9b5463e3f3814b646e31aab1a0c84b9e15c3ca7
rogerho <rogerho@umich.edu>
2023-03-15 15:04:42 -0400

    updated projects and intro mar 2023

--- src/components/header.js
+++ src/components/header.js
@@ -9,12 +9,12 @@ function Header() {
           <p style={{textAlign: 'center', paddingBottom: '1.5%'}}>rogerho [at] umich.edu | <a href="https://www.linkedin.com/in/roger-ho-576548199/">LinkedIn</a> | <a href="https://github.com/rogerksho">Github</a></p>
           <div className="intro-div">
             <div className="intro-paragraph-div">
-            <p className="intro">I am a rising senior (undergraduate) at the University of Michigan, 
+            <p className="intro">I am a senior (undergraduate) at the University of Michigan, 
             majoring in mechanical engineering and minoring in computer science. With experience in both 
             software and hardware, I aim to develop a well rounded skillset to realize sophisticated mechatronic systems.
             </p>
             <p className="intro">
-            I am currently working on the design of a high performance (large range, high speed, nanometric motion quality)
+            I am currently working on the design and testing of a flexure-based high performance (large range, high speed, nanometric motion quality)
             nanopositioning stage (HIPERNAP) under Professor <a href="https://me.engin.umich.edu/people/faculty/shorya-awtar/">Shorya Awtar</a> at the <a href="https://psdl.engin.umich.edu/">
             Precision Systems Design Laboratory</a> here at the University of Michigan.
             </p>
--- src/components/projects.js
+++ src/components/projects.js
@@ -6,10 +6,10 @@ import chamber_pic from "../img/chamber.jpg"
 function Projects() {
     return (
         <div className="projects-parent">
-            <h1 style={{padding: '2px'}}>Projects</h1>
+            <h1 style={{padding: '2px'}}>Personal Projects</h1>
             <p style={{paddingLeft: '0px'}} className="intro">
             One of my interests include understanding complex yet interesting systems and attempting to reconstruct
-            them from ground up. Another interest of mine is automating processes and daily tasks, especially those
+            them from scratch. Another interest of mine is automating processes and daily tasks, especially those
             involving growing food.
             </p>
             <div className="project-item">
@@ -17,10 +17,11 @@ function Projects() {
                 <div className="project-item-content">
                     <h4 className="project-item-title">3D Display with holographic acoustic tweezers (HAT)<text className="separator">|</text> <a href="">Github</a></h4>
                     <ul>
-                        <li className="project-item-list">Wrote Verilog HDL code Cylcone II FPGA to command phases of 50 ultrasonic transducers at &pi;/64 resolution, in addition to receiving 
+                        <li className="project-item-list">Proprietary Verilog HDL code for a Cylcone II FPGA to command phases of 50 ultrasonic transducers at &pi;/64 resolution, in addition to receiving 
                             phase commands with ESP-8266 microcontroller over SPI.</li>
-                        <li className="project-item-list">Built an Arduino based C++ library to facilitate phase calculations and communications between the FPGA and microcontroller.</li>
-                        <li className="project-item-list">Designed custom PCB in KiCAD and had it manufactured; Hand soldered SMD components </li>
+                        <li className="project-item-list">Arduino based C++ library to compute phases and facilitate communications between the FPGA and microcontroller.</li>
+                        <li className="project-item-list">Custom PCB in drawn up in KiCAD and manufactured by Aisler; Hand soldered SMD components.</li>
+                        <li className="project-item-list">Capable of displaying simple shapes such as [TO BE FIXED]</li>
                         <li className="project-item-list">Heavily inspired by "Acoustophoretic volumetric displays using a fast-moving levitated particle" by A. Marzio et al.</li>
                     </ul>
                 </div>
@@ -31,11 +32,11 @@ function Projects() {
                 <div className="project-item-content">
                     <h4 className="project-item-title">Indoor gourmet mushroom fruiting chamber<text className="separator">|</text> <a href="">Github</a></h4>
                     <ul>
-                        <li className="project-item-list">Upcycled old rack into an ESP8266 SoC (System on Chip) based fruiting chamber.</li>
-                        <li className="project-item-list">Designed and fabricated chamber to contain humidity of up to 90% while minimizing harmful microorganism growth.</li>
-                        <li className="project-item-list"></li>
-                        <li className="project-item-list">Developed a MicroPython based library to control the chamber, implementing PID regulation for humidity and 
-                        establishing a TCP connection between the ESP8266 and a "base station" PC to routinely send humidity and temperature data.</li>
+                        <li className="project-item-list">Upcycled old rack transformed into an ESP-8266 SoC (System on Chip) based fruiting chamber.</li>
+                        <li className="project-item-list">Chamber capable of maintaining humidity of up to 95% while minimizing harmful microorganism growth with UV-C irradiation.</li>
+                        <li className="project-item-list">Driven by proprietary MicroPython based library to control the chamber, with PID regulation for humidity, as well as a
+                        TCP connection between the ESP-8266 and a "base station" PC to routinely send humidity and temperature data.</li>
+                        <li className="project-item-list">Has housed many gourmet and medicinal mushroom species such as pink oyster, golden oyster, lion's mane, shiitake, red reishi (lingzhi), pioppino.</li>
                     </ul>
                 </div>
             </div>
@@ -45,8 +46,22 @@ function Projects() {
                 <div className="project-item-content">
                     <h4 className="project-item-title">Automated windowsill herb planter<text className="separator">|</text> <a href="">Github</a></h4>
                     <ul>
-                        <li className="project-item-list">Upcycled old rack into an ESP8266 SoC (System on Chip) based fruiting chamber.</li>
-                        <li className="project-item-list">Designed simple circuit with a boost converter and a transistor-driven relay to control a pump off a single voltage input.</li>
+                        <li className="project-item-list">Automated irrigation for up to 9 flower pots on the windowsill, as well as high CRI LED grow lights.</li>
+                        <li className="project-item-list">Arduino based period control of LED grow lights and irrigation pump.</li>
+                        <li className="project-item-list">Nifty tricks to overcome annoying head loss associated with cheap plastic pipe tees.</li>
+                        <li className="project-item-list">Designed simple circuit with a boost converter and a transistor-driven relay to power the pump, relay, and the controller with a single voltage input.</li>
+                        <li className="project-item-list">Monthly pesto!</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className="project-item">
+                <img className="project-gif" src={chamber_pic}/>
+                <div className="project-item-content">
+                    <h4 className="project-item-title">Peltier-cooled cloud chamber<text className="separator">|</text> <a href="">Github</a></h4>
+                    <ul>
+                        <li className="project-item-list">Upcycled old wine crate into main chassis of cloud chamber.</li>
+                        <li className="project-item-list"></li>
                         <li className="project-item-list">Developed a MicroPython based library to control the chamber, implementing PID regulation for humidity and 
                         establishing a TCP connection between the ESP8266 and a "base station" PC to routinely send humidity and temperature data.</li>
                     </ul>
