9239202b4186c5d8f4d6123b1c7cd2ae78d73d5f
rogerho <rogerho@umich.edu>
2023-11-05 18:20:21 -0500

    Updates

---
+++ precache-manifest.c3033bf30d80e31ff38bb3616b0e9406.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e1ea19926555eb53653d8cf0bfe6253",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e59e4964332f81d5242a",
+    "url": "/static/css/main.a58b7479.chunk.css"
+  },
+  {
+    "revision": "7e73e20ff942233b848f",
+    "url": "/static/js/2.83e2faca.chunk.js"
+  },
+  {
+    "revision": "2ad33e9abe21e6769bbc",
+    "url": "/static/js/3.bd282c0b.chunk.js"
+  },
+  {
+    "revision": "e59e4964332f81d5242a",
+    "url": "/static/js/main.b741334e.chunk.js"
+  },
+  {
+    "revision": "02558d794f1b309512ce",
+    "url": "/static/js/runtime~main.f27e42ef.js"
+  },
+  {
+    "revision": "799363e0dbe057484a5653561657e2f8",
+    "url": "/static/media/Bitcoin.799363e0.png"
+  },
+  {
+    "revision": "5e639711b0be71140afaf21fd19291ea",
+    "url": "/static/media/RogerResumeNov23.5e639711.pdf"
+  },
+  {
+    "revision": "c24c776f7cbf0ac2da268610b74d721a",
+    "url": "/static/media/chamber.c24c776f.jpg"
+  },
+  {
+    "revision": "2a68b1ffca68a77206c7ee687b089ec7",
+    "url": "/static/media/cloud_chamber.2a68b1ff.png"
+  },
+  {
+    "revision": "48bb6d9fd3cecf330440e913fbe953e9",
+    "url": "/static/media/face_japan_cropped.48bb6d9f.png"
+  },
+  {
+    "revision": "34a757da6a50031a809ea3a9aef679ec",
+    "url": "/static/media/planter.34a757da.png"
+  },
+  {
+    "revision": "8ba85d7aaa030099c3938b876f7b8c13",
+    "url": "/static/media/ultrasonic.8ba85d7a.gif"
+  }
+]);
