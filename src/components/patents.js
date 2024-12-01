d4c93a3c83713a4088943f13abc258255103098e
Roger <rogerksho@gmail.com>
2024-12-01 11:57:23 -0500

    comma in patent app no.

--- src/components/patents.js
+++ src/components/patents.js
@@ -6,7 +6,7 @@ function Patents() {
     return (
         <div className="projects-parent">
             <ol type="1">
-            <li>Shorya Awtar, Siddharth Rath, <b>Roger Ho</b>. 2024. Method of Manufacture and Assembly of XY Flexure Mechanism Assembly. U.S. Patent Application 18/605411, filed March 14, 2024. Patent pending.</li>
+            <li>Shorya Awtar, Siddharth Rath, <b>Roger Ho</b>. 2024. Method of Manufacture and Assembly of XY Flexure Mechanism Assembly. U.S. Patent Application 18/605,411, filed March 14, 2024. Patent pending.</li>
             </ol>
         </div>
         
